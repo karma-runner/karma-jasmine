karma-runner/karma-qunit

Initial commit, extracted from https://github.com/testacular/testacular

lib/index.js
@@ -1,0 +1,14 @@
+var createPattern = function(path) {
+  return {pattern: path, included: true, served: true, watched: false};
+};
+
+var initQUnit = function(files) {
+  files.unshift(createPattern(__dirname + '/adapter.js'));
+  files.unshift(createPattern(__dirname + '/qunit.js'));
+};
+
+initQUnit.$inject = ['config.files'];
+
+module.exports = {
+  'framework:qunit': ['factory', initQUnit]
+};

karma-runner/karma-jasmine

lib/index.js
@@ -1,0 +1,14 @@
+var createPattern = function(path) {
+  return {pattern: path, included: true, served: true, watched: false};
+};
+
+var initJasmine = function(files) {
+  files.unshift(createPattern(__dirname + '/adapter.js'));
+  files.unshift(createPattern(__dirname + '/jasmine.js'));
+};
+
+initJasmine.$inject = ['config.files'];
+
+module.exports = {
+  'framework:jasmine': ['factory', initJasmine]
+};