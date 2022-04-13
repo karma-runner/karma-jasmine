02e94797beb77fa5d230fac39c337de8f7b00426
XhmikosR <xhmikosr@gmail.com>
2022-04-13 08:01:43 +0300

    build: enforce LF and update .editorconfig

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,7 @@
 // Do not add new tasks. Grunt is used only for building and will be replaced.
 module.exports = function (grunt) {
   grunt.loadTasks('tasks')
+  grunt.util.linefeed = '\n'
   grunt.initConfig({
     pkgFile: 'package.json',
     build: {
